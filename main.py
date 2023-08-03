e6fa258653ace0e05f985e737526170a5529d652
Anonymous4045 <contact@anonymous4045.social>
2023-08-02 20:34:10 -0500

    Conform to conventions

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
-import pygame, time, random
+"""
+A simple game of snake, with a few twists.
+"""
 
-width = 15 # number of columns
-height = 10 # number of rows
+import random
+import time
 
-initial_speed = 25
-initial_speed_rate = 1 # How much the speed increases per apple eaten
+import pygame
+
+WIDTH = 15  # number of columns
+HEIGHT = 10  # number of rows
+
+INITIAL_SPEED = 25
+SPEED_INCREASE_PER_APPLE = 1
 
 pygame.init()
 
-# Assets
-snake_body = pygame.image.load('Assets/snake_body(32x32).png')
-apple = pygame.image.load('Assets/snake apple.png')
-snake_head = pygame.image.load('Assets/snake head.png')
+# assets
+SNAKE_BODY = pygame.image.load("assets/snake_body(32x32).png")
+APPLE = pygame.image.load("assets/apple.png")
+SNAKE_HEAD = pygame.image.load("assets/snake_head.png")
 
 
-pygame.display.set_caption('Snake')
-pygame.display.set_icon(pygame.image.load('Assets/snake_body.png'))
+pygame.display.set_caption("Snake")
+pygame.display.set_icon(pygame.image.load("assets/snake_body.png"))
 
-screen_size = 32 * width, 32 * height
-screen = pygame.display.set_mode(screen_size)
+SCREEN_SIZE = 32 * WIDTH, 32 * HEIGHT
+SCREEN = pygame.display.set_mode(SCREEN_SIZE)
+
+x_positions = [i for i in range(32 * WIDTH) if i % 32 == 0]
+y_positions = [i for i in range(32 * HEIGHT) if i % 32 == 0]
 
 
 # Colors
-black = 0, 0, 0
+BLACK = 0, 0, 0
 
 # Keys
 w = pygame.K_w
@@ -32,44 +42,48 @@ d = pygame.K_d
 q = pygame.K_q
 
 
-def apple_pos():
-    global head_x, head_y, apple_x, apple_y
-    x_positions = []
-    for i in range((32 * width)):
-        if i % 32 == 0:
-            x_positions.append(i)
-    y_positions = []
-    for i in range((32 * height)):
-        if i % 32 == 0:
-            y_positions.append(i)
+def make_apple() -> tuple[int, int] | None:
+    """
+    Generates a new position for the apple. If none is found, return None.
+    :return:
+    """
+
     while True:
-        rand_x, rand_y = random.randint(0, len(x_positions) - 1), random.randint(0, len(y_positions) - 1)
+        rand_x, rand_y = random.randint(0, len(x_positions) - 1), random.randint(
+            0, len(y_positions) - 1
+        )
         if x_positions[rand_x] != head_x and y_positions[rand_y] != head_y:
-            apple_x, apple_y = x_positions[rand_x], y_positions[rand_y]
-            return apple_x, apple_y
+            return x_positions[rand_x], y_positions[rand_y]
 
 
-def opposite_direction():
-    global direction
-    direction = {'up':'down', 'down':'up', 'left':'right', 'right':'left'}[direction]
+def change_direction(current_direction) -> str | None:
+    """
+    Inverts the direction of the snake.
+    :param current_direction: The direction the snake is traveling
+    :return: The new direction
+    """
+
+    return {"up": "down", "down": "up", "left": "right", "right": "left"}.get(
+        current_direction
+    )
 
 
 while True:
-    head_x, head_y = 32 * width / 2, 32 * height / 2
+    head_x, head_y = 32 * WIDTH / 2, 32 * HEIGHT / 2
     if head_x % 32 != 0:
         head_x -= head_x % 32
     if head_y % 32 != 0:
         head_y -= head_y % 32
-        
+
     apple_x, apple_y = 0, 0
 
-    apple_pos()
+    make_apple()
 
-    direction = 'right'
+    direction = "right"
 
-    speed = 10 / initial_speed
-    speed_cap = .1
-    speed_rate = 10 / initial_speed_rate
+    speed = 10 / INITIAL_SPEED
+    speed_cap = 0.1
+    speed_rate = 0.01 * SPEED_INCREASE_PER_APPLE
 
     segments = 1
 
@@ -80,77 +94,74 @@ while True:
         all_positions.append((head_x, head_y))
 
         for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("Game quit")
+                pygame.quit()
+                game_loop = False
             if event.type == pygame.KEYDOWN:
                 if event.key == q:
-                    print('Game quit')
+                    print("Game quit")
                     pygame.quit()
                     game_loop = False
-                elif event.key == a and direction != 'right':
-                    direction = 'left'
-                elif event.key == d and direction != 'left':
-                    direction = 'right'
-                elif event.key == w and direction != 'down':
-                    direction = 'up'
-                elif event.key == s and direction != 'up':
-                    direction = 'down'
+                elif event.key == a and direction != "right":
+                    direction = "left"
+                elif event.key == d and direction != "left":
+                    direction = "right"
+                elif event.key == w and direction != "down":
+                    direction = "up"
+                elif event.key == s and direction != "up":
+                    direction = "down"
 
-        if direction == 'up':
+        if direction == "up":
             head_y -= 32
-        elif direction == 'down':
+        elif direction == "down":
             head_y += 32
-        elif direction == 'left':
+        elif direction == "left":
             head_x -= 32
-        elif direction == 'right':
+        elif direction == "right":
             head_x += 32
 
-        if head_x > 32 * (width - 1):
+        if head_x > 32 * (WIDTH - 1):
             head_x = 0
         if head_x < 0:
-            head_x = 32 * (width - 1)
-        if head_y > 32 * (height - 1):
+            head_x = 32 * (WIDTH - 1)
+        if head_y > 32 * (HEIGHT - 1):
             head_y = 0
         if head_y < 0:
-            head_y = 32 * (height - 1)
+            head_y = 32 * (HEIGHT - 1)
 
         if not game_loop:
             break
 
+        print(speed)
         if head_x == apple_x and head_y == apple_y:
-            apple_pos()
+            apple_x, apple_y = make_apple()
             speed -= speed_rate
             segments += 1
-            if speed <= speed_cap:
-                speed = speed_cap
+            speed = max(speed, speed_cap)
 
-        screen.fill(black)
-        screen.blit(snake_head, (head_x, head_y))
-        screen.blit(apple, (apple_x, apple_y))
+        SCREEN.fill(BLACK)
+        SCREEN.blit(SNAKE_HEAD, (head_x, head_y))
+        SCREEN.blit(APPLE, (apple_x, apple_y))
 
         if segments > 0:
             for segment in range(segments):
                 segment_pos = all_positions[-1 * segment]
                 if segment_pos == (head_x, head_y):
                     if segment == 2:
-                        if direction == 'left':
+                        if direction == "left":
                             head_x += 32
-                            opposite_direction()
-                            pass
-                        elif direction == 'right':
+                        elif direction == "right":
                             head_x -= 32
-                            opposite_direction()
-                            pass
-                        elif direction == 'up':
+                        elif direction == "up":
                             head_y += 32
-                            opposite_direction()
-                            pass
-                        elif direction == 'down':
+                        elif direction == "down":
                             head_y -= 32
-                            opposite_direction()
-                            pass
+                        direction = change_direction(direction)
                     else:
-                        print('You died!')
+                        print("You died!")
                         game_loop = False
-                screen.blit(snake_body, segment_pos)
+                SCREEN.blit(SNAKE_BODY, segment_pos)
 
         pygame.display.flip()
         time.sleep(speed)
